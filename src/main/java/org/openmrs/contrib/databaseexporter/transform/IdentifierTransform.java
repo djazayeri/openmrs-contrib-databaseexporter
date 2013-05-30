bdad45fd4fd9a6500530d7042efe1ab063b70286
Michael Seaton <mseaton@pih.org>
2013-05-30 17:58:28 -0400

    Upgrade jackson json libraries to fix bug in deserialization.  Add very basic IdentifierTransform that will likely need to be replaced with something more sophisticated.

---
+++ src/main/java/org/openmrs/contrib/databaseexporter/transform/IdentifierTransform.java
@@ -0,0 +1,60 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.contrib.databaseexporter.transform;
+
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.openmrs.contrib.databaseexporter.ExportContext;
+import org.openmrs.contrib.databaseexporter.TableRow;
+import org.openmrs.contrib.databaseexporter.util.Util;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Transforms patient identifier type and patient identifier
+ * so that they are fully de-identified
+ */
+public class IdentifierTransform extends RowTransform {
+
+	//***** CONSTRUCTORS *****
+
+	public IdentifierTransform() {}
+
+	//***** INSTANCE METHODS *****
+
+	/**
+	 * TODO: Improve this
+	 * This probably isn't a sufficient solution, as we will want to be able to demonstrate
+	 * and test the identifier validators and use them with idgen.  For now, we will leave it
+	 * like this because it accomplishes the first most important task, which is de-identification
+	 */
+	public void applyTransform(TableRow row, ExportContext context) {
+		if (row.getTableName().equals("patient_identifier_type")) {
+			row.setRawValue("check_digit", 0);
+			row.setRawValue("validator", null);
+			row.setRawValue("format", null);
+			row.setRawValue("format_description", null);
+		}
+		if (row.getTableName().equals("patient_identifier")) {
+			row.setRawValue("identifier", row.getRawValue("patient_identifier_id"));
+		}
+	}
+
+	//***** PROPERTY ACCESS *****
+
+
+}
